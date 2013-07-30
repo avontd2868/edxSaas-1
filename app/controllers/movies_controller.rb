c11ecb80def65d2c4711b35c33971fb11a2fabc5
azhicodan <azhicodan@gmail.com>
2013-07-30 21:33:15 +0530

    session and redirection homework finished.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,31 @@ class MoviesController < ApplicationController
   attr_accessor :release_date_style
   attr_accessor :all_ratings
   attr_accessor :checked_ratings
+  attr_accessor :restful_append
+  def create_restful_uri
+    @restful_append = "?"
+    #Just to check again
+    if !params[:sort].nil?
+      session[:sort] = params[:sort]
+    end
+    if !session[:sort].nil?
+      @restful_append += "sort=#{session[:sort]}&"
+    end
+    if !session[:checked_ratings].nil?
+      session[:checked_ratings].each do |rating|
+        @restful_append += "ratings[#{rating}]=#{rating}&"
+      end
+    end
+    if @restful_append[-1] == "&"
+      @restful_append[-1] = ""
+    end
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+     create_restful_uri   
   end
   def index 
     @title_style = ""
@@ -17,42 +38,48 @@ class MoviesController < ApplicationController
     Movie.select("DISTINCT rating").each do |movie|
       @all_ratings<<movie.rating
     end
+    #Read the selected ratings and sort from params
     selected_ratings = params[:ratings]
-    if !selected_ratings.nil?      
-      flash[:ratings] = selected_ratings
-      @checked_ratings = selected_ratings.keys
-      if !flash[:sort].nil?
-        @movies = Movie.find(:all, :conditions => { :rating => selected_ratings.keys } , :order => flash[:sort].to_s + " ASC")
-        if flash[:sort] == "title"
-          @title_style = "hilite"
-        else
-          @release_date_style = "hilite"
-        end
-      else
-        @movies = Movie.find(:all, :conditions => { :rating => selected_ratings.keys })
-      end
-    else
-      @checked_ratings = @all_ratings
-      @movies = Movie.all
-    end
     sort = params[:sort]
+
+    #Set the ratings value in session
+    if !selected_ratings.nil?      
+      session[:checked_ratings] = selected_ratings.keys
+    elsif session[:checked_ratings].nil?
+      #This means user has not clicked refresh yet.
+      session[:checked_ratings] = @all_ratings
+    else
+      create_restful_uri
+      url = "/movies" + @restful_append
+      redirect_to url
+      return
+    end
+
+    #Set the sort value in session
     if !sort.nil?
-      flash[:sort] = sort
-      if sort == "title"
-        if !flash[:ratings].nil?
-          @movies = Movie.find(:all, :conditions => { :rating => flash[:ratings].keys } , :order => sort + " ASC")
-        else
-          @movies = Movie.order("title ASC")
-        end
-        @title_style = "hilite"
-      elsif sort == "release_date"
-        if !flash[:ratings].nil?
-          @movies = Movie.find(:all, :conditions => { :rating => flash[:ratings].keys } , :order => sort.to_s + " ASC")
-        else
-          @movies = Movie.order("release_date ASC")
-        end
-        @release_date_style = "hilite"
-      end
+      session[:sort] = sort
+    elsif !session[:sort].nil?
+      create_restful_uri
+      url = "/movies" + @restful_append
+      redirect_to url
+      return
+    end
+
+    #Set the style for column headers
+    case session[:sort]
+    when "title" then
+      @title_style = "hilite"
+    when "release_date" then
+      @release_date_style = "hilite"
+    end
+
+    #Set the instance variables used in view
+    @checked_ratings = session[:checked_ratings]
+    #No order clause if the session sort value is nil
+    if !session[:sort].nil?
+      @movies = Movie.find(:all, :conditions => { :rating => session[:checked_ratings] } , :order => session[:sort].to_s + " ASC")
+    else
+      @movies = Movie.find(:all, :conditions => { :rating => session[:checked_ratings] } )
     end
   end
 
