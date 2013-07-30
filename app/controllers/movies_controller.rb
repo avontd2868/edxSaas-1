0c367a14d48276395d0091f96751331c059d6931
azhicodan <azhicodan@gmail.com>
2013-07-30 14:13:06 +0530

    Homework 2 filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,26 +2,58 @@ class MoviesController < ApplicationController
 
   attr_accessor :title_style
   attr_accessor :release_date_style
-
+  attr_accessor :all_ratings
+  attr_accessor :checked_ratings
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-  def index
+  def index 
     @title_style = ""
     @release_date_style = ""
-    sort = params[:sort]
-    if sort == "title"
-      @movies = Movie.order("title ASC")
-      @title_style = "hilite"
-    elsif sort == "release_date"
-      @movies = Movie.order("release_date ASC")
-      @release_date_style = "hilite"
+    @all_ratings = []
+    @checked_ratings = []
+    Movie.select("DISTINCT rating").each do |movie|
+      @all_ratings<<movie.rating
+    end
+    selected_ratings = params[:ratings]
+    if !selected_ratings.nil?      
+      flash[:ratings] = selected_ratings
+      @checked_ratings = selected_ratings.keys
+      if !flash[:sort].nil?
+        @movies = Movie.find(:all, :conditions => { :rating => selected_ratings.keys } , :order => flash[:sort].to_s + " ASC")
+        if flash[:sort] == "title"
+          @title_style = "hilite"
+        else
+          @release_date_style = "hilite"
+        end
+      else
+        @movies = Movie.find(:all, :conditions => { :rating => selected_ratings.keys })
+      end
     else
+      @checked_ratings = @all_ratings
       @movies = Movie.all
     end
-      
+    sort = params[:sort]
+    if !sort.nil?
+      flash[:sort] = sort
+      if sort == "title"
+        if !flash[:ratings].nil?
+          @movies = Movie.find(:all, :conditions => { :rating => flash[:ratings].keys } , :order => sort + " ASC")
+        else
+          @movies = Movie.order("title ASC")
+        end
+        @title_style = "hilite"
+      elsif sort == "release_date"
+        if !flash[:ratings].nil?
+          @movies = Movie.find(:all, :conditions => { :rating => flash[:ratings].keys } , :order => sort.to_s + " ASC")
+        else
+          @movies = Movie.order("release_date ASC")
+        end
+        @release_date_style = "hilite"
+      end
+    end
   end
 
   def new
